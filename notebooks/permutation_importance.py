2d38f88d513f810b79da421f1b8f07b3e3d53203
Brian DeCost <brian.decost@nist.gov>
2021-04-23 10:30:54 -0400

    add seaborn boxplot of random feature rank over replicates, sorted by training AUC score.

--- notebooks/permutation_importance.py
+++ notebooks/permutation_importance.py
@@ -4,6 +4,7 @@ import re
 from pathlib import Path
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 replicates = 25
@@ -106,7 +107,6 @@ for (idx, row), r_path in zip(df.iterrows(), results):
 perf = pd.DataFrame(perf)
 rf_rank = pd.DataFrame(rf_rank)
 
-
 fig, ax = plt.subplots()
 c = {
     "min": rf_rank.min(axis=1),
@@ -125,3 +125,37 @@ plt.tight_layout()
 st.pyplot(fig)
 
 # perf.shape
+
+fig, ax = plt.subplots()
+y = rf_rank.mean(axis=1)
+yerr = (y - rf_rank.min(axis=1), rf_rank.max(axis=1) - y)
+p = ax.scatter(perf.train_NPCA_auc, c)
+p = ax.scatter(perf.train_NPCA_auc, y)
+ax.errorbar(
+    perf.train_NPCA_auc,
+    y,
+    yerr=yerr,
+    color="k",
+    linestyle="none",
+)
+plt.xlabel("train AUC")
+plt.ylabel("RF rank")
+plt.tight_layout()
+
+# ax.scatter(df.max_depth, df.min_leaf_sample, c=perf.train_precision)
+st.pyplot(fig)
+
+# box plot of random feature ranks
+# groups/runs sorted by increasing train AUC
+order = np.argsort(perf.train_NPCA_auc)
+_rank = rf_rank.iloc[order]
+_rank = _rank.reset_index(drop=True).T
+
+# rearrange to make one seaborn boxplot per column
+# see https://stackoverflow.com/a/46134162
+_rankdata = _rank.melt(var_name="run", value_name="RF Rank")
+
+fig, ax = plt.subplots(figsize=(16, 4))
+sns.boxplot(x="run", y="RF Rank", data=_rankdata)
+plt.plot(_rank.mean(axis=0), color="k")
+st.pyplot(fig)
