69a3554a40fb6783b050188e316827406d574821
Brian DeCost <brian.decost@nist.gov>
2021-04-30 17:00:27 -0400

    wrap shapley + impurity script for repeated shuffle-split cross validation replicates.

--- src/models/rf_shapley.py
+++ src/models/rf_shapley.py
@@ -1,4 +1,4 @@
-import sys
+from dataclasses import dataclass
 from enum import Enum
 from typing import Literal, Optional
 
@@ -9,6 +9,7 @@ import shap
 import typer
 from sklearn import ensemble, pipeline, preprocessing
 from sklearn.metrics import roc_auc_score, roc_curve
+from tqdm import tqdm
 
 from src.data import load_hea_dataset, split_chemical_systems
 from src.features import randcat
@@ -19,10 +20,88 @@ class Target(str, Enum):
     intermetallic = "IM"
 
 
+@dataclass
+class Dataset:
+    X: np.ndarray
+    y: np.ndarray
+
+
+@dataclass
+class Result:
+    auc_train: float
+    auc_val: float
+    auc_test: float
+    impurity_importance: np.ndarray
+    shapley_importance: np.ndarray
+
+
+def fit_replicate(
+    model,
+    df: pd.DataFrame,
+    data: Dataset,
+    testdata: Dataset,
+    n_random_features: int = 1,
+    verbose: bool = False,
+):
+
+    # random feature augmentation
+    data.X = randcat(data.X, n_random_features=n_random_features)
+    testdata.X = randcat(testdata.X, n_random_features=n_random_features)
+
+    # train/val split
+    train_ids, val_ids = split_chemical_systems(df, verbose=verbose)
+    train = Dataset(data.X[train_ids], data.y[train_ids])
+    val = Dataset(data.X[val_ids], data.y[val_ids])
+
+    # standardization not needed for models without PCA...
+    # # feature standardization
+    # if np.std(train.X, axis=0).min() == 0:
+    #     print("bad standardization!")
+    # std = preprocessing.StandardScaler()
+
+    # std.fit(train.X)
+
+    # train.X = std.transform(train.X)
+    # val.X = std.transform(val.X)
+    # testdata.X = std.transform(testdata.X)
+
+    model.fit(train.X, train.y)
+    feature_importances = model.feature_importances_
+
+    def compute_roc(model, X, y):
+        scores = model.predict_proba(X)[:, 1]
+        return roc_auc_score(y, scores)
+
+    r = {
+        "auc_train": compute_roc(model, train.X, train.y),
+        "auc_val": compute_roc(model, val.X, val.y),
+        "auc_test": compute_roc(model, testdata.X, testdata.y),
+        "impurity_importance": feature_importances,
+    }
+
+    impurity_rank = np.argsort(feature_importances)[::-1]
+    if verbose:
+        print("random feature impurity rank: ", impurity_rank[-1])
+
+    # aggregated shapley importance
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer(train.X)
+    shap_values = shap_values[..., 1]  # just values for positive class
+    aggregated_shap = np.abs(shap_values.data).mean(axis=0)
+    shap_rank = np.argsort(aggregated_shap)[::-1]
+    if verbose:
+        print("random feature shap rank: ", shap_rank[-1], aggregated_shap[-1])
+
+    r["shapley_importance"] = aggregated_shap
+
+    return Result(**r)
+
+
 def main(
     target: Target = Target.multiphase,
     max_depth: Optional[int] = None,
     min_samples_leaf: int = 1,
+    replicates: int = 1,
     n_random_features: int = 1,
 ):
     target_key = f"PROPERTY: {target.value}"
@@ -30,19 +109,11 @@ def main(
 
     df, X = load_hea_dataset(subset="train")
     y = df[target_key].values
+    data = Dataset(X, y)
 
-    df_acta, X_test = load_hea_dataset(subset="test")
-    y_test = df_acta[target_key].values
-
-    train, val = split_chemical_systems(df)
-    Xrand = randcat(X, n_random_features=n_random_features)
-
-    # feature standardization
-    std = preprocessing.StandardScaler()
-    std.fit(Xrand[train])
-    Xrand = std.transform(Xrand)
-
-    X_test = std.transform(randcat(X_test, n_random_features=n_random_features))
+    df_acta, X_acta = load_hea_dataset(subset="test")
+    y_acta = df_acta[target_key].values
+    testdata = Dataset(X_acta, y_acta)
 
     model = ensemble.RandomForestClassifier(
         n_estimators=144,
@@ -52,32 +123,11 @@ def main(
         class_weight="balanced",
     )
 
-    model.fit(Xrand[train], y[train])
-    feature_importances = model.feature_importances_
+    results = []
+    for replicate in tqdm(range(replicates)):
+        results.append(fit_replicate(model, df, data, testdata))
 
-    train_scores = model.predict_proba(Xrand[train])[:, 1]
-    train_auc = roc_auc_score(y[train], train_scores)
-
-    val_scores = model.predict_proba(Xrand[val])[:, 1]
-    val_auc = roc_auc_score(y[val], val_scores)
-
-    test_scores = model.predict_proba(X_test)[:, 1]
-    test_auc = roc_auc_score(y_test, test_scores)
-
-    print(f"{train_auc=}")
-    print(f"{val_auc=}")
-    print(f"{test_auc=}")
-
-    impurity_rank = np.argsort(feature_importances)[::-1]
-    print("random feature impurity rank: ", impurity_rank[-1])
-
-    # aggregated shapley importance
-    explainer = shap.TreeExplainer(model)
-    shap_values = explainer(Xrand)
-    shap_values = shap_values[..., 1]  # just values for positive class
-    aggregated_shap = np.abs(shap_values.data[train]).mean(axis=0)
-    shap_rank = np.argsort(aggregated_shap)[::-1]
-    print("random feature shap rank: ", shap_rank[-1], aggregated_shap[-1])
+    print(pd.DataFrame(results))
 
 
 if __name__ == "__main__":
